96abf0a10cc5c0b8fbc02430c4591463b50aea32
mohammed sweif <sweif4949@gmail.com>
2020-06-03 19:36:28 +0200

    DeleteComment

--- final project core/final project core/Final project/Startup.cs
+++ final project core/final project core/Final project/Startup.cs
@@ -66,20 +66,6 @@ namespace Final_project
             services.AddScoped<VendorWorksRepsitory>();
 
 
-            /************************************************************/
-            /*   
-                 services.AddAuthentication(options =>
-                 {
-                     options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                     options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                     options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                 }).AddCookie(options =>
-                 {
-                     options.Cookie.HttpOnly = true;
-                     options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
-                     options.LogoutPath = "/Account/logout";
-                     options.SlidingExpiration = true;
-                 });*/
             services.AddAuthentication(options =>
             {
                 options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
